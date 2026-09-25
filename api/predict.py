3a67e1a2deb74f18eb209cbc2aa8c701a0c27a1d
Madhan Kumar <madhankumarofficial007@gmail.com>
2026-09-25 14:45:05 +0530

    Update predict.py

--- api/predict.py
+++ api/predict.py
@@ -2,16 +2,25 @@ from http.server import BaseHTTPRequestHandler
 import json
 
 class handler(BaseHTTPRequestHandler):
+
     def do_POST(self):
-        response = {
-            "crop": "Tomato",
-            "disease": "Late Blight",
-            "confidence": "97.3%",
-            "treatment": "Apply Copper Fungicide"
+
+        response={
+
+            "crop":"Tomato",
+
+            "disease":"Late Blight",
+
+            "confidence":"97.8%",
+
+            "treatment":"Apply Copper Fungicide"
+
         }
 
         self.send_response(200)
+
         self.send_header("Content-Type","application/json")
+
         self.end_headers()
+
         self.wfile.write(json.dumps(response).encode())
-      
